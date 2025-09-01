8eed367860f598089d40ef9ead7a27da4a8c23f6
shalevohad <shalev.ohad@gmail.com>
2025-09-01 13:39:57 +0300

    Fix build error - add referance to System.Linq

--- Common/Network/Server/HttpServer.cs
+++ Common/Network/Server/HttpServer.cs
@@ -9,6 +9,7 @@ using NLog;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.Json;
