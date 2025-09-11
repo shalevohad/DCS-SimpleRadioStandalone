6b8cf10585c2128ee599e4bf35d60cf335934374
shalevohad <shalev.ohad@gmail.com>
2025-09-11 23:32:55 +0300

    browsing controller - adding a reset button to return to the default folder, changing the text box to a label and displaying the name of the selected folder in the content + displaying the full path of the folder in the tool tip,

--- DCS-SR-Client/UI/ClientWindow/BrowseFolderControl.xaml.cs
+++ DCS-SR-Client/UI/ClientWindow/BrowseFolderControl.xaml.cs
@@ -1,11 +1,15 @@
+using Ciribob.DCS.SimpleRadio.Standalone.Common.Settings;
 using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
 
 namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.ClientWindow
 {
-    public partial class BrowseFolderControl : System.Windows.Controls.UserControl
+    public partial class BrowseFolderControl : System.Windows.Controls.UserControl, INotifyPropertyChanged
     {
         public static readonly DependencyProperty PathProperty =
             DependencyProperty.Register(nameof(Path), typeof(string), typeof(BrowseFolderControl), new PropertyMetadata(""));
@@ -13,16 +17,47 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.ClientWindow
         public static readonly DependencyProperty DialogTitleProperty =
             DependencyProperty.Register(nameof(DialogTitle), typeof(string), typeof(BrowseFolderControl), new PropertyMetadata("Select Directory"));
 
+        public static readonly DependencyProperty DefaultPathProperty =
+            DependencyProperty.Register(nameof(DefaultPath), typeof(string), typeof(BrowseFolderControl), new PropertyMetadata(""));
+
         public string Path
         {
             get => (string)GetValue(PathProperty);
-            set => SetValue(PathProperty, value);
+            set
+            {
+                SetValue(PathProperty, value);
+                // Notify UI that DirectoryName changed
+                OnPropertyChanged(nameof(DirectoryName));
+            }
         }
 
         public string DialogTitle
         {
             get => (string)GetValue(DialogTitleProperty);
             set => SetValue(DialogTitleProperty, value);
+        }
+
+        public string DefaultPath
+        {
+            get => (string)GetValue(DefaultPathProperty);
+            set => SetValue(DefaultPathProperty, value);
+        }
+
+        public string DirectoryName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Path))
+                    return Directory.GetCurrentDirectory(); //current working directory
+                try
+                {
+                    return System.IO.Path.GetFileName(Path);
+                }
+                catch
+                {
+                    return Path;
+                }
+            }
         }
 
         public BrowseFolderControl()
@@ -36,20 +71,31 @@ namespace Ciribob.DCS.SimpleRadio.Standalone.Client.UI.ClientWindow
             {
                 dialog.Description = DialogTitle;
 
-                // Resolve initial path: if not rooted, combine with executable directory
                 var initialPath = Path;
+                #region Resolve initial path: if not rooted, combine with executable directory
                 if (!string.IsNullOrWhiteSpace(initialPath) && !System.IO.Path.IsPathRooted(initialPath))
                 {
-                    string exeDir = System.AppDomain.CurrentDomain.BaseDirectory;
+                    string exeDir = Directory.GetCurrentDirectory();
                     initialPath = System.IO.Path.Combine(exeDir, initialPath);
                 }
-
-                if (!string.IsNullOrWhiteSpace(initialPath) && System.IO.Directory.Exists(initialPath))
-                    dialog.SelectedPath = initialPath;
+                #endregion
+                dialog.SelectedPath = initialPath;
 
                 if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                     Path = dialog.SelectedPath;
             }
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Path = DefaultPath; //back to default path
+        }
+
+        // Add this method to raise property changed notifications
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
